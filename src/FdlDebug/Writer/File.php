c1b9ecd34d595ae9238b8a1c5f75069e525d5856
Franz <franz.deleon@triplecanopy.com>
2014-06-20 14:35:09 -0400

    added time in file writer

--- src/FdlDebug/Writer/File.php
+++ src/FdlDebug/Writer/File.php
@@ -13,6 +13,7 @@ class File implements WriterInterface
         $trace = $debug->findTraceKeyAndSlice($debug->getBackTrace(), 'function', '__call', 2);
         $file  = $trace[0]['file'];
         $line  = $trace[0]['line'];
+        $time  = date('y-m-d h:i:s');
 
         switch ($extra['function']) {
             case 'printObject':
@@ -20,7 +21,7 @@ class File implements WriterInterface
             case 'printBackTrace':
             case 'printFiles':
             default:
-                $string  = PHP_EOL . "******************************TOP ({$file}:{$line})**********************************" . PHP_EOL;
+                $string  = PHP_EOL . "******************************TOP ({$file}:{$line} at {$time})**********************************" . PHP_EOL;
                 $string .= print_r($content, true);
                 $string .= PHP_EOL . "*****************************BOTTOM********************************" . PHP_EOL;
 
