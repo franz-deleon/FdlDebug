76e389daf8db0068eda00691aebdee0a8f5db5b0
franz-deleon <kelmadics@gmail.com>
2022-08-17 22:11:13 -0400

    added function_exists

--- Functions.php
+++ Functions.php
@@ -12,49 +12,60 @@ use FdlDebug\Bootstrap;
  * @see \FdlDebug\Debug::pr();
  * @param mixed $value
  */
-function pr($value) {
-    return Front::i()->pr($value);
+if (! function_exists("pr")) {
+    function pr($value) {
+        return Front::i()->pr($value);
+    }
 }
 
 /**
  * @see \FdlDebug\Debug::prDie();
  * @param mixed $value
  */
-function prd($value = '') {
-    return Front::i()->prDie($value);
+if (! function_exists("prd")) {
+    function prd($value = '') {
+        return Front::i()->prDie($value);
+    }
 }
 
 /**
  * @see \FdlDebug\Debug::pr();
  * @param mixed $value
  */
-function pr_now($value) {
-    return Front::i()->pr($value);
+if (! function_exists("pr_now")) {
+    function pr_now($value) {
+        return Front::i()->pr($value);
+    }
 }
 
 /**
  * @see \FdlDebug\Debug::printGlobal();
  * @param string $type
  */
-function pr_global($type = null)
-{
-    return Front::i()->printGlobal($type);
+if (! function_exists("pr_global")) {
+    function pr_global($type = null) {
+        return Front::i()->printGlobal($type);
+    }
 }
 
 /**
  * @see \FdlDebug\Debug::printBackTrace();
  * @param boolean $show_vendor
  */
-function pr_backtrace($show_vendor = false) {
-    return Front::i()->printBackTrace($show_vendor);
+if (! function_exists("pr_backtrace")) {
+    function pr_backtrace($show_vendor = false) {
+        return Front::i()->printBackTrace($show_vendor);
+    }
 }
 
 /**
  * @see \FdlDebug\Debug::printFiles();
  * @param string $show_vendor
  */
-function pr_files($show_vendor = false) {
-    return Front::i()->printFiles($show_vendor);
+if (! function_exists("pr_files")) {
+    function pr_files($show_vendor = false) {
+        return Front::i()->printFiles($show_vendor);
+    }
 }
 
 ###### XDebug function(s) ######
@@ -64,11 +75,13 @@ function pr_files($show_vendor = false) {
  * @param string  $search
  * @param boolean $show_vendor
  */
-function prx_trace_var($search, $show_vendor = false) {
-    $fdl_debug = Front::i();
-    $fdl_debug->printXdebugTracedVar($search, $show_vendor);
+if (! function_exists("prx_trace_var")) {
+    function prx_trace_var($search, $show_vendor = false) {
+        $fdl_debug = Front::i();
+        $fdl_debug->printXdebugTracedVar($search, $show_vendor);
 
-    return $fdl_debug;
+        return $fdl_debug;
+    }
 }
 
 ######### Conditions ###########
@@ -77,11 +90,13 @@ function prx_trace_var($search, $show_vendor = false) {
  * @see \FdlDebug\Condition\Boolean::condBoolean()
  * @param boolean $condition
  */
-function cond_bool($condition) {
-    $fdl_debug = Front::i();
-    $fdl_debug->condBoolean($condition);
+if (! function_exists("cond_bool")) {
+    function cond_bool($condition) {
+        $fdl_debug = Front::i();
+        $fdl_debug->condBoolean($condition);
 
-    return $fdl_debug;
+        return $fdl_debug;
+    }
 }
 
 /**
@@ -89,26 +104,30 @@ function cond_bool($condition) {
  * @param int $start
  * @param int $length
  */
-function cond_range($start, $length = null) {
-    $fdl_debug = Front::i();
-    $fdl_debug->setBacktraceFuncToSearch(__FUNCTION__);
-    $fdl_debug->loopRange($start, $length);
-    $fdl_debug->setBacktraceFuncToSearch(Front::BACKTRACE_FUNC_TO_SEARCH); // reset after use
+if (! function_exists("cond_range")) {
+    function cond_range($start, $length = null) {
+        $fdl_debug = Front::i();
+        $fdl_debug->setBacktraceFuncToSearch(__FUNCTION__);
+        $fdl_debug->loopRange($start, $length);
+        $fdl_debug->setBacktraceFuncToSearch(Front::BACKTRACE_FUNC_TO_SEARCH); // reset after use
 
-    return $fdl_debug;
+        return $fdl_debug;
+    }
 }
 
 /**
  * @param void
  * @return Ambigous <\FdlDebug\Front, \FdlDebug\FdlDebug\Front>
  */
-function cond_range_nested_end() {
-    $fdl_debug = Front::i();
-    $fdl_debug->setBacktraceFuncToSearch(__FUNCTION__);
-    $fdl_debug->rangeNestedEnd();
-    $fdl_debug->setBacktraceFuncToSearch(Front::BACKTRACE_FUNC_TO_SEARCH); // reset after use
+if (! function_exists("cond_range_nested_end")) {
+    function cond_range_nested_end() {
+        $fdl_debug = Front::i();
+        $fdl_debug->setBacktraceFuncToSearch(__FUNCTION__);
+        $fdl_debug->rangeNestedEnd();
+        $fdl_debug->setBacktraceFuncToSearch(Front::BACKTRACE_FUNC_TO_SEARCH); // reset after use
 
-    return $fdl_debug;
+        return $fdl_debug;
+    }
 }
 
 /**
@@ -116,35 +135,41 @@ function cond_range_nested_end() {
  * @param string  $expression
  * @param int     $length
  */
-function cond_from($expression, $length = null) {
-    $fdl_debug = Front::i();
-    $fdl_debug->setBacktraceFuncToSearch(__FUNCTION__);
-    $fdl_debug->loopFrom($expression, $length);
-    $fdl_debug->setBacktraceFuncToSearch(Front::BACKTRACE_FUNC_TO_SEARCH); // reset after use
+if (! function_exists("cond_from")) {
+    function cond_from($expression, $length = null) {
+        $fdl_debug = Front::i();
+        $fdl_debug->setBacktraceFuncToSearch(__FUNCTION__);
+        $fdl_debug->loopFrom($expression, $length);
+        $fdl_debug->setBacktraceFuncToSearch(Front::BACKTRACE_FUNC_TO_SEARCH); // reset after use
 
-    return $fdl_debug;
+        return $fdl_debug;
+    }
 }
 
 /**
  * @param void
  * @return Ambigous <\FdlDebug\Front, \FdlDebug\FdlDebug\Front>
  */
-function cond_from_nested_end() {
-    $fdl_debug = Front::i();
-    $fdl_debug->loopFromNestedEnd();
+if (! function_exists("cond_from_nested_end")) {
+    function cond_from_nested_end() {
+        $fdl_debug = Front::i();
+        $fdl_debug->loopFromNestedEnd();
 
-    return $fdl_debug;
+        return $fdl_debug;
+    }
 }
 
 /**
  * @see \FdlDebug\Extension\LoopFrom::loopFromFlush()
  * @param void
  */
-function cond_from_flush() {
-    $fdl_debug = Front::i();
-    $fdl_debug->loopFromFlush();
+if (! function_exists("cond_from_flush")) {
+    function cond_from_flush() {
+        $fdl_debug = Front::i();
+        $fdl_debug->loopFromFlush();
 
-    return $fdl_debug;
+        return $fdl_debug;
+    }
 }
 
 ############# Misc functions #############
@@ -153,17 +178,20 @@ function cond_from_flush() {
  * Retrieves the instance of fdl debug
  * @return Front
  */
-function fd_i($writer = null) {
-    return Front::i($writer);
+if (! function_exists("fd_i")) {
+    function fd_i($writer = null) {
+        return Front::i($writer);
+    }
 }
 
 /**
  * Retrieves the writer instance
  * @return \FdlDebug\Writer
  */
-function fd_writer()
-{
-    return Front::i()->getWriter();
+if (! function_exists("fd_writer")) {
+    function fd_writer() {
+        return Front::i()->getWriter();
+    }
 }
 
 ################## DO NOT EDIT BELLOW HERE #################
